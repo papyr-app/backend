d13e34203a643d8c951206a9a38e3af1706ca8e8
Erik Wessman <erikwessman10@gmail.com>
2024-05-06 20:02:23 +0200

    Added document collection to track seen and unseen annotations

---
+++ papyr/src/models/seen_status.py
@@ -0,0 +1,16 @@
+from mongoengine import Document, ReferenceField, DateTimeField
+from datetime import datetime
+
+from models.user import User
+from models.annotation import Annotation
+
+
+class SeenStatus(Document):
+    user = ReferenceField(User, required=True)
+    annotation = ReferenceField(Annotation, required=True)
+    seen_at = DateTimeField(default=datetime.utcnow)
+
+    meta = {
+        'collection': 'seen_statuses',
+        'ordering': ['-seen_at']
+    }
