a06b4d0478b3785688250ba57cbe8e6873950ae9
Erik Wessman <erikwessman10@gmail.com>
2024-06-02 21:35:09 +0200

    Added logging for eb

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,6 +1,10 @@
 from gevent import monkey
 monkey.patch_all()
 
+import logging
 from app import init_app, socketio
 
 app = init_app("config.ProductionConfig")
+
+if __name__ != '__main__':
+    logging.info('Starting app...')
