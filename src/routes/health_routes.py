837a627c63519fa5959246410618b0c99a253e80
Erik Wessman <erikwessman10@gmail.com>
2024-06-02 21:45:39 +0200

    Or maybe not

--- src/routes/health_routes.py
+++ src/routes/health_routes.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 
 
 def create_health_bp():
-    health_bp = Blueprint('health', __name__)
+    health_bp = Blueprint('health', __name__, url_prefix='/api/health')
 
-    @health_bp.route('/', methods=['GET'])
+    @health_bp.route('', methods=['GET'])
     def health_check():
         return "OK", 200
 
