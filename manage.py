b28ecdd2c2a5c49a75dc42c4439185dcb5c2a949
Erik Wessman <erikwessman10@gmail.com>
2024-07-11 10:08:49 +0200

    Fixed upgrade script

--- manage.py
+++ manage.py
@@ -4,6 +4,3 @@ from src.app import init_app, db
 
 app = init_app("src.config.DevelopmentConfig")
 migrate = Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run()
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python manage.py db upgrade
+export FLASK_APP=manage.py
+flask db upgrade
 gunicorn -k gevent -w 1 -b 0.0.0.0:8000 wsgi:app
