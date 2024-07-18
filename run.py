b00166bf2f1bb25275115bbdbe8ba0db60439239
Erik Wessman <erikwessman10@gmail.com>
2024-07-18 17:03:34 +0200

    Added dev dockerfile and added/modified scripts for starting

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ from src.app import init_app, socketio
 if __name__ == "__main__":
     app = init_app("src.config.DevelopmentConfig")
     logging.info("Starting app...")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+#
+./scripts/wait-for-it.sh db:5432 --timeout=60 --strict -- echo "Database is up"
+
 export FLASK_APP=manage.py
 flask db upgrade
-gunicorn -k gevent -w 1 -b 0.0.0.0:8000 wsgi:app
+
+if [ "$1" == "dev" ]; then
+  python run.py
+else
+  gunicorn -k gevent -w 1 -b 0.0.0.0:8000 wsgi:app
+fi
