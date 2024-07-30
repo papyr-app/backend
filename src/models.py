9dc55f554fef562d9ec72c075b761d420ed9748e
Erik Wessman <erikwessman10@gmail.com>
2024-07-30 22:25:10 +0200

    Fixed wrong number of chars for table ID

--- src/models.py
+++ src/models.py
@@ -9,11 +9,11 @@ document_collaborators = db.Table(
     "document_collaborators",
     db.Column(
         "pdf_document_id",
-        db.String(32),
+        db.String(36),
         db.ForeignKey("pdf_documents.id", ondelete="CASCADE"),
         primary_key=True,
     ),
-    db.Column("user_id", db.String(32), db.ForeignKey("users.id"), primary_key=True),
+    db.Column("user_id", db.String(36), db.ForeignKey("users.id"), primary_key=True),
 )
 
 
